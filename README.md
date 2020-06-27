f92bbd7d426d5076e2792ea08c65f44a054642b4
Lina Sachuk <sachuklina@gmail.com>
2020-06-26 21:45:30 -0600

    added files

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# SuperHeroes
+# flask_app
+
+ETL is a type of data integration that refers to the three steps (extract, transform, load) used to blend data from multiple sources. It's often used to build a data warehouse. During this process, data is taken (extracted) from a source system, converted (transformed) into a format that can be analyzed, and stored (loaded) into a data warehouse or other system. Extract, load, transform (ELT) is an alternate but related approach designed to push processing down to the database for improved performance.
+
+ETL Project - The Best 50 SuperHero Movies Heroes Data
+
+Project Proposal - To create a data set that will united Best 50 SuperHero Movies info with movie's SuperHero info.
+
+Extract Data - I - there are four sources of data that I extracted from:
+
+     1. superhero API (to get SuperHeroes info)
+     2. data scraped from the web (to ger best 50 SuperHeroes Movies)
+     3. omdb API (to get detail info about best 50 SuperHeroes movies)
+     4. python package - IMDbPY (to get info about main Actor and the main Role(SuperHero))
+
+![Screenshot](Screenshots/2.png)
+
+II - there are four ways how the data was formatted: 1. CSV 2. XLSX 3. pgAdmin 4. HTML
+
+Transform Data - the type of transformation needed for this data:
+
+      1. cleaning
+      2. joining
+      3. filtering
+      4. aggregating (groupby, sort, average, count)
+      5. deleting columns
+      6. data visualization
+
+![Screenshot](Screenshots/3.png)
+
+![Screenshot](Screenshots/4.png)
+
+![Screenshot](Screenshots/5.png)
+
+7.  data virtualization (created a Flask Application)
+
+![Screenshot](Screenshots/1.png)
+
+8. Deployed my App to Heroku : https://etl-project-super-heroes.herokuapp.com/
+
+   Jupyter Notebook HTML version with whole ETL process available under link ETL-Project(Jupyter Notebook) in the App navigation bar .
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
---
+++ app.py
@@ -0,0 +1,81 @@
+import os
+import csv
+from flask import request
+from flask import Flask, render_template, url_for, template_rendered
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+
+def remove_file(filepath):
+    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+# from nbconvert import HTMLExporter
+# import codecs
+# import nbformat
+# exporter = HTMLExporter()
+# output_notebook = nbformat.read('ETL-Project.ipynb', as_version=4)
+# output, resources = exporter.from_notebook_node(output_notebook)
+# codecs.open('test.html', 'w', encoding='utf-8').write(output)
+
+
+app = Flask(__name__)
+
+csv_path = os.path.join('movie_actor_hero copy.csv')
+
+
+movies_list = []
+with open(csv_path, newline='') as csv_file:
+
+    csv_data = csv.reader(csv_file, delimiter=',')
+    # print(csv_data)
+
+    csv_header = next(csv_data)
+    # print(f"csv_header: {csv_header}")
+
+    count = 0
+    for row in csv_data:
+        movies = {}
+        # print('================================')
+
+        # print(count)
+        movies['Movie_Rank'] = row[5]
+        movies['Title'] = row[0]
+        movies['Plot'] = row[3]
+        movies['Poster'] = row[4]
+        movies['Hero_Name'] = row[7]
+        movies['Hero_Images'] = row[12]
+        count += 1
+
+        movies_list.append(movies)
+# print('================================')
+
+sorted_list = sorted(movies_list, key=lambda k: int(k['Movie_Rank']))
+# print(sorted_list)
+# print('================================')
+
+
+@app.route('/')
+def index():
+
+    # @TODO: render an index.html template and pass it the data you retrieved from the database
+
+    return render_template("index.html", list=sorted_list)
+
+
+# @app.route('/ETL')
+# def etl():
+#      return render_template("ETL-Project.html")
+
+
+# @app.route('/<string:page_name>/')
+# def render_static("ETL-Project.html"):
+#     return render_template('%s.html' % "ETL-Project.html")
+
+@app.route('/ETL')
+def etl():
+    return app.send_static_file('ETL-Project.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
